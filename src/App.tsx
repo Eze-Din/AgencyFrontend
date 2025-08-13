376febbe910bbfc47b4362a903ea58db74c5268b
Eze Din <70455313+Eze-Din@users.noreply.github.com>
2025-08-13 12:08:28 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword";
-import Dashboard from "./pages/Dashboard";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/forgot" element={<ForgotPassword />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
